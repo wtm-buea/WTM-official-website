5b689f4ebda1802fa3e1c403bf510d2a43928068
alicendeh <alicendeh16@gmail.com>
2021-10-14 10:26:37 +0100

    Added the  bootstrap cdn

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
-import logo from './logo.png';
-import './App.css';
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      <nav>
-        <a>
-          <img src={logo} className="App-logo" alt="logo" />
-          </a>
-          <ul>
-            <li><a class="active">Home</a></li>
-            <li><a>About</a></li>
-            <li><a>Events</a></li>
-            <li><a>Success Stories</a></li>
-            <li><a>Contact</a></li>
-          </ul>
-        </nav>
-      </header>
-    </div>
-  );
+  return <div className="bg-primary">Happy Coding</div>;
 }
 
 export default App;
