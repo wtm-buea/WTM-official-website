1adbed6ecb427d4d913414590d5622cc044ed6dc
alicendeh <alicendeh16@gmail.com>
2021-10-16 11:28:33 +0100

    .

--- src/Example/Example.jsx
+++ src/Example/Example.jsx
@@ -5,7 +5,7 @@ function Example() {
     <div>
       <p className="p-tag bg-secondary">
         This file is to show the way you have to organize your files when
-        contributing. Happy coding
+        contributing. Happy coding!!
       </p>
     </div>
   );
