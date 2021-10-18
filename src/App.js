8a5b04a3803441bd5fc029349d022d6efcc24188
alicendeh <alicendeh16@gmail.com>
2021-10-18 18:33:38 +0100

    added some annimations packages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
 import Example from "./Example/Example";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 function App() {
-  return <Example />;
+  AOS.init();
+  return (
+    <div data-aos="fade-right" data-aos-delay="500">
+      hey alice ndeh
+    </div>
+  );
 }
 
 export default App;
--- src/Example/Example.jsx
+++ src/Example/Example.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Example.css";
+import { Wave } from "react-animated-text";
+import { Random } from "react-animated-text";
+
 function Example() {
+  const [pause, setpause] = useState(false);
+  setTimeout(() => {
+    setpause(true);
+  }, 1000);
   return (
     <div>
       <p className="p-tag bg-secondary">
         This file is to show the way you have to organize your files when
         contributing. Happy coding!!
       </p>
+      {/* <Wave text="EXAMPLE TEXT" /> */}
+      {/* <Wave text="EXAMPLE TEXT" effect="" effectChange={2.2} /> */}
+      <Random
+        text="sup alice"
+        paused={pause}
+        effect="verticalFadeOut"
+        effectDirection="down"
+        effectChange={7.0}
+      />
     </div>
   );
 }
