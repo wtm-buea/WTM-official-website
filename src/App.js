8ac377ffb8dd8db6c91ac153b8a3b6f9dc87519e
alicendeh <alicendeh16@gmail.com>
2021-10-19 18:11:41 +0100

    worked in the responsiveness of component 4

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 //import Example from "./Example/Example";
+import Hero from "./components/LandingPage/ComponentTwo/hero/hero.jsx";
+import COmp3 from "./components/LandingPage/ComponentThree/Main";
 import Comp4 from "./components/LandingPage/ComponentFour/MainComp4";
 function App() {
   return <Comp4 />;
