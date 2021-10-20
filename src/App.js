0a5597f20eaef5361e1d27d7b852287cdc30d041
Lemuelle Baya <balemuelle@gmail.com>
2021-10-20 22:17:22 +0100

    created the navigation bar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
 import Hero from "./components/LandingPage/ComponentTwo/hero/hero";
+import Navbar from "./components/LandingPage/ComponentOne/Navbar/Navbar";
 function App() {
   return (
     <div>
+      <Navbar/>
+      <Hero />
       <Hero />
     </div>
   );
--- src/components/LandingPage/ComponentOne/Navbar/Navbar.js
+++ src/components/LandingPage/ComponentOne/Navbar/Navbar.js
@@ -1,5 +1,30 @@
-import React from "react";
+import React,  { useState, useEffect } from "react";
 
-export default function Navbar() {
-  return <div>{/* !your navbar code comes in here */}</div>;
+import Wtmlogo from '../../../../Assets/icons/logo.png';
+
+(window).scroll(function(){
+('nav').toggleClass('scrolled', (this).scrollTop() > 800)
+});
+
+function Navbar(){
+  (window).scroll(function(){
+    ('nav').toggleClass('scrolled', (this).scrollTop() > 800)
+    });
+    
+  return(
+    <div>
+      <nav className="bg-transparent" class="navbar fixed-top navbar-expand-sm navbar-dark">
+        <a class="navbar-brand" href="#">
+        <img src={Wtmlogo} style={{width:200, height:70}}></img>
+        </a>
+        <ul class="navbar-custom navbar-nav ml-auto">
+          <li class="nav-item active"><a class="nav-link">Home<span class="sr-only">(current)</span></a></li>
+          <li class="nav-item"><a class="nav-link">Events</a></li>
+          <li class="nav-item"><a class="nav-link">Success Stories</a></li>
+        </ul>
+ 
+      </nav>
+    </div>
+  );
 }
+export default Navbar
