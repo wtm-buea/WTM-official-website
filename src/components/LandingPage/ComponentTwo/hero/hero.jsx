c4c577f42ca4d654f8788fc6c6f04c149c3603cc
xennia316 <xennia316@gmail.com>
2022-07-26 19:00:22 +0100

    hero section

--- src/components/LandingPage/ComponentTwo/hero/hero.jsx
+++ src/components/LandingPage/ComponentTwo/hero/hero.jsx
@@ -1,21 +1,23 @@
 import React from "react";
-import "./hero.css";
+import styles from "./hero.module.css";
 
 function Hero() {
   return (
     <div>
-      <div className="img-fluid">
-        <div className="jumbotron text-center">
-          <div className="container">
-            <h1 className="text display-4">
-              Women shouldn't be afraid to put themselves forward.
+      <div className={styles.imgFluid}>
+        <div className={`${styles.jumbotron} ${styles.TextCenter}`}>
+          <div className={styles.container}>
+            <h1 className={styles.textDisplay4}>
+              Women shouldn't be afraid to <br /> put themselves forward.
             </h1>
-            <h4 className="text">
-              WTM <i class="bi-star-fill"></i> Buea
-            </h4>
-            <h4 className="text">
-              <i class="bi bi-dash-lg"></i> 2015 <i class="bi bi-dash-lg"></i>
-            </h4>
+            <div className={styles.bottomTextDiv}>
+              <div className={styles.bottomText}> WTM </div>
+              <div className={styles.bottomTextMid}>
+                <div className={styles.innerBottomTextDiv}>★</div>
+                <div className={styles.innerBottomTextDiv}>2015</div>
+              </div>
+              <div className={styles.bottomText}>Buea</div>
+            </div>
           </div>
         </div>
       </div>
