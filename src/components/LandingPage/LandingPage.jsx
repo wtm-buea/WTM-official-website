9c6ff04061790cf2cbfbcfefbe6cd53e0a904b61
Nasah-Kuma <nasahnash19@gmail>
2022-08-15 14:47:22 +0100

    made the achievements section responsive

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,7 +1,7 @@
 import Comp1 from "./ComponentOne/Navbar/Navbar";
 import Comp2 from "./ComponentTwo/hero/hero";
 import Comp3 from "./ComponentThree/Main";
-import Comp4 from "./ComponentFour/MainComp4";
+import Comp4 from "./ComponentFour/Comp4";
 import Comp5 from "./ComponentFive/ComponentFive";
 import Comp6 from "./ComponentSix/vision/vision";
 import Comp7 from "./FooterComponent/FooterComponent";
@@ -13,8 +13,8 @@ function LandingPage() {
             <Comp1 />
             <Comp2 />
             <Comp3 />
-            {/*<Comp4 />
-            <Comp5 />
+            <Comp4 />
+            {/*<Comp5 />
             <Comp6 />
             <Comp7 /> */}
         </>
